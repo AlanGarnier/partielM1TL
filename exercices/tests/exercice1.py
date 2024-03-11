083631eebd50b68d01c6620fcf99674c296effb2
AlanGarnier <a.garnier164@gmail.com>
2024-03-11 10:01:25 +0100

    First commit

---
+++ exercices/tests/exercice1.py
@@ -0,0 +1,22 @@
+import unittest
+
+
+# Function
+def add(x, y):
+    return x + y
+
+
+# Test case pour tester la fonction add
+class MyTestCase(unittest.TestCase):
+    def test_add_positive_number(self):
+        self.assertEqual(add(10, 15), 25)
+
+    def test_add_negative_number(self):
+        self.assertEqual(add(-2, -2), -4)
+
+    def test_add_zero(self):
+        self.assertEqual(add(0, 0), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ exercices/tests/exercice3.py
@@ -0,0 +1,31 @@
+import unittest
+from unittest.mock import MagicMock
+
+inventory = {'apples': 30, 'bananas': 15, 'oranges': 10}
+
+
+def update_inventory(item, quantity):
+    if item in inventory:
+        inventory[item] += quantity
+    else:
+        inventory[item] = quantity
+
+
+class MyTestInventory(unittest.TestCase):
+    def test_update_inventory(self):
+        # On copie de l'inventaire existant
+        inventory_copy = inventory.copy()
+
+        # Mock de la fonction update_inventory
+        update_inventory_mock = MagicMock(side_effect=update_inventory)
+
+        with unittest.mock.patch('__main__.update_inventory', update_inventory_mock):
+            update_inventory('bananas', -2)  # Ajouter 2 oranges
+            update_inventory_mock.assert_called_once_with('bananas', -2)
+
+            # On vérifie que l'inventaire n'a pas été modifié
+            self.assertEqual(inventory, inventory_copy)
+
+
+if __name__ == '__main__':
+    unittest.main()
